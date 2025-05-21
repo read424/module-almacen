0b57ceef171cb76dd488e533f5759e6d07b194a0
Ronald E. Aybar D <read424@gmail.com>
2025-05-21 12:37:20 -0500

    feat: prueba unitarias para patron Strategy kardex registro

--- src/main/java/com/walrex/module_almacen/domain/model/enums/TipoOrdenIngreso.java
+++ src/main/java/com/walrex/module_almacen/domain/model/enums/TipoOrdenIngreso.java
@@ -1,4 +1,8 @@
 package com.walrex.module_almacen.domain.model.enums;
 
 public enum TipoOrdenIngreso {
+    LOGISTICA_GENERAL,
+    TELA_CRUDA,
+    OTRO_TIPO// Otros tipos que puedas necesitar en el futuro
+    ;
 }
