475171388da74eba5bcc6541ac0dda055f9a8fef
Ronald E. Aybar D <read424@gmail.com>
2025-05-27 10:43:08 -0500

    feat: domain business by approve shipping or deliver

--- src/main/java/com/walrex/module_almacen/domain/model/dto/AprobarSalidaRequerimientoDTO.java
+++ src/main/java/com/walrex/module_almacen/domain/model/dto/AprobarSalidaRequerimientoDTO.java
@@ -1,4 +1,27 @@
 package com.walrex.module_almacen.domain.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AprobarSalidaRequerimientoDTO {
+    private Integer idOrdenSalida;
+    private String codOrdenSalida;
+    private Integer idRequerimiento;
+    private Integer idTipoComprobante;
+    private Integer idAlmacenOrigen;
+    private Integer idAlmacenDestino;
+    private Integer idUsuarioEntrega;
+    private String entregado;
+    private Integer idUsuarioSupervisor;
+    private Date fecEntrega;
+    private List<ArticuloRequerimientoDTO> detalles;
 }
