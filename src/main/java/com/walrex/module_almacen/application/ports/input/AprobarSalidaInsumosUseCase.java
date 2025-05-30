b868d0b2c81e9057094a44c57e7d3a43044b4f3d
Ronald E. Aybar D <read424@gmail.com>
2025-05-29 22:21:41 -0500

    fix: cambios para completar el service para entrega de requerimiento

--- src/main/java/com/walrex/module_almacen/application/ports/input/AprobarSalidaInsumosUseCase.java
+++ src/main/java/com/walrex/module_almacen/application/ports/input/AprobarSalidaInsumosUseCase.java
@@ -1,10 +1,11 @@
 package com.walrex.module_almacen.application.ports.input;
 
 import com.walrex.module_almacen.domain.model.dto.AprobarSalidaRequerimiento;
+import com.walrex.module_almacen.domain.model.dto.ResponseAprobacionRequerimientoDTO;
 import com.walrex.module_almacen.infrastructure.adapters.inbound.reactiveweb.dto.AprobarSalidaRequestDTO;
 import com.walrex.module_almacen.infrastructure.adapters.inbound.reactiveweb.dto.AprobarSalidaResponseDTO;
 import reactor.core.publisher.Mono;
 
 public interface AprobarSalidaInsumosUseCase {
-    Mono<AprobarSalidaRequerimiento> aprobarSalidaInsumos(AprobarSalidaRequerimiento request);
+    Mono<ResponseAprobacionRequerimientoDTO> aprobarSalidaInsumos(AprobarSalidaRequerimiento request);
 }
